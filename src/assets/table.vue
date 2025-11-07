51560e4975a2f7df6ae3e4d6165efe7ee96da344
chris-breum <chrisbreum3032@gmail.com>
2025-11-07 09:39:08 +0100

    Auto-update: 7.11.2025, 09.39.08

--- src/assets/table.vue
+++ src/assets/table.vue
@@ -44,14 +44,21 @@ const isCell1Duplicate = (value, index) => {
         <thead>
         
             <tr>
-             <td class="py-2 px-4 border-b border-r border-gray-600 w-1/16"></td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-4/16">Row 1, Cell 1</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/16">Row 1, Cell 2</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/16">Row 1, Cell 3</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/16">Row 1, Cell 4</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/16">Row 1, Cell 5</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/16">Row 1, Cell 6</td>
-            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/16">Row 1, Cell 7</td>
+             <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18"></td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-4/18">Row 1, Cell 1</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 2</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 3</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 4</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 5</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 6</td>
+             <td class="py-2 px-4 border-b border-r border-gray-600 w-4/18">Row 1, Cell 1</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 2</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 3</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 4</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 5</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 6</td>
+              <td class="py-2 px-4 border-b border-r border-gray-600 w-1/18">Row 1, Cell 6</td>
+            <td class="py-2 px-4 border-b border-r border-gray-600 w-2/18">Row 1, Cell 7</td>
             </tr>
 
         </thead>
