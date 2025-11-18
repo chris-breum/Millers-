7000768d3af0d6fb2c2040bdc6dc005b3e806ae1
chris-breum <chrisbreum3032@gmail.com>
2025-11-18 11:34:43 +0100

    Auto-update: 18.11.2025, 11.34.43

--- SQLQueryDB.sql
+++ SQLQueryDB.sql
@@ -33,4 +33,33 @@ ALTER TABLE Measurements
 	ADD CONSTRAINT Measurements_MeasurementTemplates_Fk
 	FOREIGN KEY (MeasurementTemplatesId) REFERENCES MeasurementTemplates (MeasurementTemplatesId);
 
+GO
+
+-- Insert dummy images
+INSERT INTO Images (Img)
+VALUES 
+    (0x0102030405),
+    (0x1122334455),
+    (0xFFEEDDCCBB);
+
+-- Insert dummy measurement templates
+INSERT INTO MeasurementTemplates (Name, ImagesId)
+VALUES
+    ('Template A', 1),
+    ('Template B', 2),
+    ('Template C', NULL), -- no image
+    ('Template D', 3);
+
+-- Insert dummy measurements
+INSERT INTO Measurements (Name, Code, MeasurementTemplatesId)
+VALUES
+    ('Length', 'LEN', 1),
+    ('Width', 'WID', 1),
+    ('Height', 'HGT', 2),
+    ('Diameter', 'DIA', 2),
+    ('Circumference', 'CIR', 3),
+    ('Weight', 'WGT', 4);
+
+GO
+
 
